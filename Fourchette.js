bd3ae5d902007c6360a181f6248efcb12998dec0
Anthosoft <anthony.bianay@hotmail.fr>
2018-03-04 21:21:38 +0100

    Add files via upload
    finisih

--- Fourchette.js
+++ Fourchette.js
@@ -5,21 +5,23 @@ const cheerio = require('cheerio');
 
 
 function JsonPromo(name,postal){
+    //The first request use the la fourchette api to get the id of the restaurant
     var urlRestaurant = "https://m.lafourchette.com/api/restaurant-prediction?name="+name;
     request(urlRestaurant, function(error,response,html){
         if(response != 'undefined'){
-        if(response.statusCode == 200){
+        if(response.statusCode == 200){//this status code means, there are no problem
             
-            var json= JSON.parse(html)
+            var json= JSON.parse(html)// The api return a json file, that is why we have to parse it 
             if(json[0]){
                 json.forEach(elementRest => {
+                    //Thanks to the name of the restaurant and the postal code I can get the the promotion of the strred restaurant using a seconde request
                     if (elementRest.address.postal_code==postal){
                     var urlPromo = "https://m.lafourchette.com/api/restaurant/"+ elementRest.id+"/sale-type";
                     request(urlPromo, function(error,response,html){
                         
                         var json = JSON.parse(html);
                         json.forEach(elementMenu => {
-                        if (elementMenu.is_special_offer) {
+                        if (elementMenu.is_special_offer) {//If the restaurant have promotion, it should be displayed on the console
                             console.log(elementMenu.title + "      "+ elementRest.name)
                         }
                     
@@ -45,7 +47,7 @@ function JsonPromo(name,postal){
 
 }
 
-for (var i=0; i<615;i++){
+for (var i=0; i<615;i++){//this loop permit to initialize the funtion 
     if(json[i]!= undefined){
     JsonPromo(json[i].nom,json[i].postal)
     }
--- Michelin.js
+++ Michelin.js
@@ -6,7 +6,7 @@ var app     = express();
 var json    = [];
 var i       = 1;
 
-function restaurants(i){
+function restaurants(i){ //This function display into a json all the information about the starred restaurant of the michelin
 
     if (i > 36){
         fs.writeFile('restaurants.json', JSON.stringify(json), function (err) {
@@ -19,17 +19,18 @@ function restaurants(i){
     request(url, function(error, response, html){
         if(!error){
             var $ = cheerio.load(html);
-
+             
             $('[attr-gtm-type = poi]').each(function(i, elem){
                 var restau = {nom: "", prix: "", offre: "", genre: "", postal: ""};
                 var data = $(elem);
                 restau.nom = data.attr("attr-gtm-title");
-    
+                
                 restau.postal = data.find()
                 restau.prix = data.find('.poi_card-display-price').text();
                 restau.offre = data.find('.mtpb2c-offers').text();
                 restau.genre = data.find('.poi_card-display-cuisines').text();
                 var link = $(elem).find("a").attr('href');
+                //To catch the postal code i node, request the link of the guide michelin restaurant
                 request("https://restaurant.michelin.fr/"+link, function(error, response, html){
                     $ = cheerio.load(html);
                     var postal = $('.postal-code').first().text().trim()
@@ -44,7 +45,7 @@ function restaurants(i){
         }
 
         i++;
-        restaurants(i)
+        restaurants(i)// it is a call back function 
     });
 }
 
